53fefd6d6d056050d3d193f16cb215d1dd46ab9d
Grover <sgrover@cs.princeton.edu>
2016-01-04 11:22:04 -0500

    add sys.argv interface to pass pcap file path

--- src/pcapFeatureExtractor.py
+++ src/pcapFeatureExtractor.py
@@ -1,5 +1,6 @@
+from __future__ import division
 from scapy.all import *
-import time
+import time, sys
 from collections import defaultdict, Counter
 
 
@@ -201,4 +202,17 @@ class pcapSummary:
 
 if __name__ == "__main__":
     #sniff(prn=test_pktfeaturizer)
-    pcapSummary().run("/Users/sgrover/Projects/IoT/IoT-dumps/smartthings/1447984949.pcap")
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--help":
+            print "Usage: python {} -r pcapFile".format(sys.argv[0])
+        elif sys.argv[1] == "-r":
+            try:
+                pcap = sys.argv[2]
+                pcapSummary().run(pcap)
+            except:
+                print "Usage: python {} -r pcapFile".format(sys.argv[0])
+        else:
+            print "Usage: python {} -r pcapFile".format(sys.argv[0])
+    return
+
